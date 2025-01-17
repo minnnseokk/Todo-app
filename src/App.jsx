88b35bd2cc40f83fd7ca3390c6a5e019d14fde38
jiwon0226 <ppg0226@naver.com>
2025-01-17 09:52:04 +0900

    Color Box 컴포넌트 생성

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ function App() {
         <TodoInsert />
         <TodoList />
       </TodoTemplate>
+      <ColorBox />
     </TodoProvider>
   );
 }
---
+++ src/ColorBox.jsx
@@ -0,0 +1,9 @@
+export default function ColorBox() {
+    return(
+        <>
+            <div>
+                <h1>컬러 박스</h1>
+            </div>
+        </>
+    )
+} 
